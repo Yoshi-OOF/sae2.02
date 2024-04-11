bc55952a3869d6bfbb3a0067851c7fcd4b225b73
Maximaximus72 <72976426+Maximaximus72@users.noreply.github.com>
2024-04-11 14:40:36 +0200

    Update Node.java

--- sae/graph/Node.java
+++ sae/graph/Node.java
@@ -1,48 +1,52 @@
 package sae.graph;
+
 import java.util.HashSet;
 import java.util.Set;
-
 import sae.dungeon.Coord;
 
+
 public class Node {
-    private String name;
-    private Set<Node> neighbors;
-    private Coord coord;
-
-    public Node(String name, Coord coord) {
-        this.name = name;
-        this.coord = coord;
-        this.neighbors = new HashSet<>();
-    }
-
-    public void addNeighbour(Node node) {
-        neighbors.add(node);
-    }
-
-    public String toString() {
-        return "Node{name='" + name + "', neighbors=" + neighbors + ", coord=" + coord + '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Coord getCoord() {
-        return coord;
-    }
-
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Node node = (Node) o;
-        return name.equals(node.name);
-    }
-
-    public int hashCode() {
-        return name.hashCode();
-    }
-    
+	private String name;
+	private Set<Node> neighbors;
+	private Coord coord;
+	
+	public Node(String name, Coord coords) {
+		super();
+		this.coord = coords;
+		this.name = name;
+		neighbors = new HashSet<>();
+	}
+	
+	public void addNeighbour(Node node) {
+		neighbors.add(node);
+	}
+	
 	public Set<Node> neighbors() {
 		return neighbors;
 	}
+	
+	public void addNeighbors(Node node) {
+		neighbors.add(node);	
+	}
+	
+	public String toString() {
+        return "Node{name='" + name + "', neighbors=" + neighbors + ", coord=" + coord + '}';
+    }
+	
+	public Coord getCoords() {
+		return coord;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Node node = (Node) object;
+        return name.equals(node.name);
+    }
+	
+
 }
