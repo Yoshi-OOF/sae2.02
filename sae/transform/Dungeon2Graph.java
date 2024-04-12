49fffee7da254574f0fd3a02904c2d3c372b3297
Yoshi <andreolienzo@gmail.com>
2024-04-12 09:50:17 +0200

    Update Dungeon2Graph.java

--- sae/transform/Dungeon2Graph.java
+++ sae/transform/Dungeon2Graph.java
@@ -5,9 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import sae.dungeon.Coord;
+import sae.dungeon.Direction;
 import sae.dungeon.Dungeon;
+import sae.dungeon.DungeonSoluce;
 import sae.dungeon.Room;
 import sae.graph.Graph;
+import sae.graph.GraphSoluce;
 import sae.graph.Node;
 
 public class Dungeon2Graph {
@@ -39,10 +43,52 @@ public class Dungeon2Graph {
 		return couples.get(room);
 	}
 	
+	public Room mappedRoom(Node node) {
+		for (Room room : couples.keySet()) {
+			if (couples.get(room).equals(node)) {
+				return room;
+			}
+		}
+		return null;
+	}
+	
 	@Override
     public String toString() {
         return graph.toString();
     }
+
+	/*
+	Explication :
+	On parcourt la liste des noeuds de la solution du graphe en partant de la fin.
+	Pour chaque noeud, on récupère le noeud suivant.
+	On récupère les salles correspondantes à ces deux noeuds.
+	On récupère la direction entre ces deux salles.
+	On ajoute cette direction à la solution.
+	/*/
+	public DungeonSoluce transform(GraphSoluce soluceGraph) {
+		DungeonSoluce soluce = new DungeonSoluce();
+		List<Node> nodes = soluceGraph.getSoluce();
+		for (int i = nodes.size() - 1; i > 0; i--) {
+			Node current = nodes.get(i);
+			Node next = nodes.get(i - 1);
+			Room currentRoom = mappedRoom(current);
+			Room nextRoom = mappedRoom(next);
+			Direction direction = directionBetween(currentRoom, nextRoom);
+			soluce.addDirection(direction);
+		}
+		return soluce;
+	}
+
+	private Direction directionBetween(Room currentRoom, Room nextRoom) {
+		for (Direction direction : currentRoom.getNextRooms().keySet()) {
+			if (currentRoom.getNextRooms().get(direction).equals(nextRoom)) {
+				return direction;
+			}
+		}
+		return null;
+	}
+	
+	
 	
 
 }
