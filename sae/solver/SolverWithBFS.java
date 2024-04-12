dd81ef8fcb06f09460f4c668b1c1e3fb46c8f807
Maximaximus72 <72976426+Maximaximus72@users.noreply.github.com>
2024-04-12 09:21:12 +0200

    Update SolverWithBFS.java

--- sae/solver/SolverWithBFS.java
+++ sae/solver/SolverWithBFS.java
@@ -1,60 +1,54 @@
 package sae.solver;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import sae.graph.GraphSoluce;
 import sae.graph.Node;
 
-import java.util.*;
+public class SolverWithBFS implements Solver{
 
-// A chaque fois qu'il passe dans un noeud il marque le previous de ce noeud (getPrevious) et à la fil il a plus qu'a remonter les prédécessseurs pour trouver le chemin
-// Constructeur : SolverWithBFS(Node start, Node end)
-public class SolverWithBFS implements Solver {
-	private Node start;
-	private Node end;
+	private Node nodeA;
+	private Node nodeB;
 	private GraphSoluce soluce;
-	private int steps;
-
-	public SolverWithBFS(Node start, Node end) {
-		this.start = start;
-		this.end = end;
+	
+	public SolverWithBFS(Node A, Node B){
+		this.nodeA = A;
+		this.nodeB = B;
 		this.soluce = new GraphSoluce();
-		this.steps = 0;
+		
 	}
-
-	@Override
+	
 	public void resolve() {
-		Queue<Node> queue = new LinkedList<>();
-		Set<Node> visited = new HashSet<>();
-		Map<Node, Node> previous = new HashMap<>();
-		queue.add(start);
-		visited.add(start);
-		while (!queue.isEmpty()) {
-			Node current = queue.poll();
-			if (current.equals(end)) {
-				Node node = current;
-				while (node != null) {
-					soluce.add(node);
-					node = previous.get(node);
-				}
-				return;
-			}
-			for (Node neighbor : current.neighbors()) {
-				if (!visited.contains(neighbor)) {
-					queue.add(neighbor);
-					visited.add(neighbor);
-					previous.put(neighbor, current);
+		List<Node> file = new LinkedList<Node>();
+		List<Node> marque = new ArrayList<Node>();
+		marque.add(nodeA);
+		file.add(nodeA);
+		while (!file.isEmpty()) {
+			Node current = file.remove(0);	
+			for (Node voisin : current.neighbors()) {
+				if (!marque.contains(voisin)) {
+					voisin.setPrevious(current);
+					marque.add(voisin);
+					file.add(voisin);
 				}
 			}
-			steps++;
 		}
+		Node current = nodeB;
+		soluce.add(current);
+		while (!current.equals(nodeA)) {
+			current = nodeB.getPrevious();
+			soluce.add(current);
+			
+		}
+		
 	}
-
-	@Override
+	
 	public GraphSoluce getSoluce() {
 		return soluce;
 	}
-
-	@Override
-	public int getSteps() {
-		return steps;
-	}
+	
+	
+	
 }
